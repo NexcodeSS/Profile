c27e6d1aa43ca31daac4091839165bc16287ecf2
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-10-23 12:25:34 -0300

    navbar funcional

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,5 @@
+// App.js
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./screens/Home";
 import Nosotros from "./screens/Nosotros";
@@ -5,16 +7,34 @@ import Servicios from "./screens/Servicios";
 import Contacto from "./screens/Contacto";
 
 function App() {
-  
+  const [section, setSection] = useState("home");
+
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+      setSection(sectionId);
+    }
+  };
+
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Nosotros/>
-      <Servicios/>
-      <Contacto/>
+      <Navbar scrollToSection={scrollToSection} />
+      <div id="home">
+        <Home />
+      </div>
+      <div id="nosotros">
+        <Nosotros />
+      </div>
+      <div id="servicios">
+        <Servicios />
+      </div>
+      <div id="contacto">
+        <Contacto />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,9 +1,39 @@
- const Navbar = () => {
-    return(
-        <div className="w-full h-[10vh] bg-[black] fixed z-50">
-            <h4>Navbarrrr</h4>
+import Icono from "../assets/imagenes/icono.png";
+const Navbar = ({ scrollToSection }) => {
+    return (
+        <div className=" flex w-full h-[10vh] bg-[black] fixed z-50 text-white">
+            <div className="flex items-center w-[20%] pl-5">
+                <img src={Icono} alt="" className="h-[90%]" />
+                <h1 className="text-2xl">NEXCODE</h1>
+            </div>
+            <div className="flex w-[60%] items-center justify-center">
+                <p onClick={() => scrollToSection("home")} className="px-10">
+                    Inicio
+                </p>
+                <p
+                    onClick={() => scrollToSection("nosotros")}
+                    className="px-10"
+                >
+                    Nosotros
+                </p>
+                <p
+                    onClick={() => scrollToSection("servicios")}
+                    className="px-10"
+                >
+                    Servicios
+                </p>
+                <p
+                    onClick={() => scrollToSection("contacto")}
+                    className="px-10"
+                >
+                    Contacto
+                </p>
+            </div>
+            <div className="flex justify-center w-[20%]">
+                
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
